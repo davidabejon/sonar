6cee3312cffbcdeec5c42b2b55276078c760a15d
David Abejón <davidabejonheras@gmail.com>
2026-05-15 18:00:37 +0200

    Add favicon and sonar.png asset

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
+        <link rel="icon" href="/sonar.png" />
         <meta name="viewport" content="width=device-width,initial-scale=1,viewport-fit=cover" />
         <GlobalStyles />
       </head>
